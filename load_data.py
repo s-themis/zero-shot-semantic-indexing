54e45951a793c04142a364d97e5a32c49363242a
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-09-28 13:25:19 +0300

    Added example of loading data as dict or dataframe

---
+++ load_data.py
@@ -0,0 +1,20 @@
+import json
+import pandas as pd
+
+from collections import Counter
+
+with open('data/test_2006.json') as f:
+    data = json.load(f)
+
+df = pd.DataFrame.from_records(data['documents'])
+
+name_counts = Counter()
+for names in df['Descriptor_names']:
+    name_counts.update(names)
+
+ui_counts = Counter()
+for uis in df['Descriptor_UIs']:
+    ui_counts.update(uis)
+
+print(name_counts.most_common(10))
+print(ui_counts.most_common(10))
