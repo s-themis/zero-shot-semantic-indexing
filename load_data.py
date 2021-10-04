e4fc083be7f1d4de8ff736036f246e4f33d1e358
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-10-04 10:14:54 +0300

    Minor changes

--- load_data.py
+++ load_data.py
@@ -7,14 +7,14 @@ with open('data/test_2006.json') as f:
     data = json.load(f)
 
 df = pd.DataFrame.from_records(data['documents'])
+print(f"Number of documents: {len(df)}")
 
 name_counts = Counter()
 for names in df['Descriptor_names']:
     name_counts.update(names)
+print(name_counts.most_common(10))
 
 ui_counts = Counter()
 for uis in df['Descriptor_UIs']:
     ui_counts.update(uis)
-
-print(name_counts.most_common(10))
 print(ui_counts.most_common(10))
