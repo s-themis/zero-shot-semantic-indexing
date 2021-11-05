07f900d7bf5200b4bb5798bc466e2c78a34e0a68
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-11-05 16:15:53 +0200

    Added script to keep track of runs

---
+++ runs.py
@@ -0,0 +1,27 @@
+from zssi.datagen import JsonlDocGenerator
+from zssi.embed import Embedder
+from zssi.parse import SentenceSegmentationDocParser, WholeTextDocParser
+from zssi.write import JsonlEmbeddingWriter
+
+
+def run_1():
+
+    modelhub_model = 'microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract'
+    pooling_mode = 'cls'
+    path_to_jsonl_data = 'data/test_2006_text_only.jsonl'
+    path_to_jsonl_output = 'data/test_2006_microsoft_BiomedNLP-PubMedBERT-base-uncased-abstract_cls_whole_text.jsonl'
+    batch_size = 10
+
+    doc_parser = WholeTextDocParser()
+    datagen = JsonlDocGenerator(path_to_jsonl=path_to_jsonl_data,
+                                doc_parser=doc_parser)
+    embedder = Embedder(modelhub_path=modelhub_model,
+                        pooling_mode=pooling_mode)
+    writer = JsonlEmbeddingWriter(path_to_jsonl=path_to_jsonl_output,
+                                  n_objs_per_write=1000)
+    embedder.embed(datagen=datagen, batch_size=batch_size, writer=writer)
+
+
+if __name__ == "__main__":
+
+    run_1()
