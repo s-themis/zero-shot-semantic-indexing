7b575bae0243db546a6460785165ae93d03200d2
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-11-09 01:35:54 +0200

    Added class to write embeddings in pickle format

--- zssi/write.py
+++ zssi/write.py
@@ -1,3 +1,5 @@
+import pickle
+
 import jsonlines as jsonl
 
 from collections import deque
@@ -14,6 +16,7 @@ class EmbeddingWriter:
         """Conducts any necessary initialization (e.g. open file) before writing can begin.
 
         """
+        raise NotImplementedError
 
     def write(self, embeddings, doc_ids):
         """Writes embeddings of correspoding documents.
@@ -33,6 +36,7 @@ class EmbeddingWriter:
         Returns:
             None
         """
+        raise NotImplementedError
 
     def finish(self):
         """Finish by writing any remaining data.
@@ -124,3 +128,87 @@ class JsonlEmbeddingWriter(EmbeddingWriter):
 
         self.writer.close()
         self.f.close()
+
+
+class PickleEmbeddingWriter(EmbeddingWriter):
+    """A class to write embeddings of documents in pickle format."
+    
+    """
+    def __init__(self, path_to_pkl, n_objs_per_file):
+        """Initializes the writer with a path to a pickle file.
+
+        Args:
+            path_to_pkl (str): The path to a pickle file in which to write the embeddings.
+        """
+        self.path_to_pkl = path_to_pkl.split(".")[0]
+        self.n_objs_per_file = n_objs_per_file
+
+        self.received_embeddings = deque()
+        self.received_doc_ids = deque()
+        self.current_embeddings = []
+        self.current_doc_id = None
+        self.objs_to_write = []
+        self.error_doc_ids = set()
+        self.total_written_objs = 0
+        self.total_written_files = 0
+
+    def _write(self):
+        filename = self.path_to_pkl + "_" + str(
+            self.total_written_files) + ".pkl"
+        with open(filename, 'wb') as f:
+            pickle.dump(self.objs_to_write, f)
+        self.total_written_objs += len(self.objs_to_write)
+        self.total_written_files += 1
+        self.objs_to_write = []
+        print(f"Total written objs: {self.total_written_objs}")
+
+    def start(self):
+        pass
+
+    def write(self, embeddings, doc_ids):
+
+        self.received_embeddings.extend(embeddings)
+        self.received_doc_ids.extend(doc_ids)
+
+        while self.received_doc_ids:
+            if self.current_doc_id is None:
+                self.current_doc_id = self.received_doc_ids.popleft()
+                self.current_embeddings.append(
+                    self.received_embeddings.popleft().tolist())
+            elif self.received_doc_ids[0] == self.current_doc_id:
+                self.current_embeddings.append(
+                    self.received_embeddings.popleft().tolist())
+                self.received_doc_ids.popleft()
+            else:
+                if self.current_doc_id in self.error_doc_ids:
+                    self.current_embeddings = None
+                    self.error_doc_ids.remove(self.current_doc_id)
+
+                self.objs_to_write.append({
+                    "pmid": self.current_doc_id,
+                    "embeddings": self.current_embeddings
+                })
+
+                self.current_doc_id = None
+                self.current_embeddings = []
+                if len(self.objs_to_write) == self.n_objs_per_file:
+                    self._write()
+
+    def log_errors(self, doc_ids):
+        self.error_doc_ids.add(doc_ids)
+
+    def finish(self):
+        if self.current_doc_id is not None:
+            if self.current_doc_id in self.error_doc_ids:
+                self.current_embeddings = None
+                self.error_doc_ids.remove(self.current_doc_id)
+
+            self.objs_to_write.append({
+                "pmid": self.current_doc_id,
+                "embeddings": self.current_embeddings
+            })
+            self.current_doc_id = None
+            self.current_embeddings = []
+
+        if self.objs_to_write:
+            self._write()
