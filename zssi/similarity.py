bdc016d7bb61255bfdc01f803ed506580b7d5930
Themistoklis Spanoudis <themistoklis.spanoudis@gmail.com>
2021-11-26 00:04:48 +0200

    Add calculation of min, mean, and max similarity

--- zssi/similarity.py
+++ zssi/similarity.py
@@ -14,32 +14,56 @@ def calculate_similarities(path_to_doc_embeddigs_dir,
                            path_to_descr_embeddings_dir,
                            path_to_similarities_dir):
 
-    #doc_parsing_type = path_to_doc_embeddigs_dir.split("/")[-1]
-    #descriptors_flavor = path_to_descr_embeddings_dir.split("/")[-1]
-
+    # Load all emerging-fine grained descriptors (embeddings) in memory
     descriptors = []
     descriptor_files = glob.glob(path_to_descr_embeddings_dir + "/*.jsonl")
+
     for file in descriptor_files:
+
         with jsonl.open(file) as f:
+
             for descr in f:
+                # covnert embedding to np.array
                 descr["embedding"] = np.array(descr["embedding"])
                 descriptors.append(descr)
 
+    # Load documents (embeddings) file by file and calculate similarities with every descriptor
     doc_files = glob.glob(path_to_doc_embeddigs_dir + "/*.pkl")
+
     for file in doc_files:
+
         with open(file, "rb") as f:
+            # Load only one file at a time in memory
             documents = pickle.load(f)
+
             for doc in tqdm.tqdm(documents):
+                # convert embeddings to np.array
                 doc["embeddings"] = list(
                     map(lambda x: np.array(x), doc["embeddings"]))
+
+                # calculate similarities of one document with every descriptor
                 for descr in descriptors:
+                    # similarities of each sentence
                     doc[descr["UI"]] = list(
                         map(lambda x: cosine_similarity(x, descr["embedding"]),
                             doc["embeddings"]))
+                    # min similarity
+                    doc[descr["UI"] + "_min"] = min(doc[descr["UI"]])
+                    # mean similarity
+                    doc[descr["UI"] + "_mean"] = sum(doc[descr["UI"]]) / len(
+                        doc[descr["UI"]])
+                    # max similarity
+                    doc[descr["UI"] + "_max"] = max(doc[descr["UI"]])
+
+                # delete embeddings to save only the similarities
                 del doc["embeddings"]
+
+        # save similarities
         file_no = file.split("/")[-1].split(".")[0]
-        with open(path_to_similarities_dir + f"/{file_no}.pkl", "wb") as f:
-            pickle.dump(documents, f)
+
+        # with open(path_to_similarities_dir + f"/{file_no}.pkl", "wb") as f:
+        #    pickle.dump(documents, f)
+
         with jsonl.open(path_to_similarities_dir + f"/{file_no}.jsonl",
                         "w") as f:
             f.write_all(documents)
